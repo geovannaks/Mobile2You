6d6684506a33c753539de2c2176ba3788b8a70c9
geovannaks <geovanna.kasemirinski@gmail.com>
2022-07-21 16:55:52 -0300

    Initial Commit

---
+++ Mobile2You/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile2You
+//
+//  Created by Geovanna Kasemirinski da Silva on 21/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
